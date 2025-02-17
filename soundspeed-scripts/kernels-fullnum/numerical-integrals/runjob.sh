51c4206bbbf5fdc5a2f9037fdabd5b27e475483e
francesco <fverdian@sissa.it>
2025-02-17 13:59:29 +0100

    tests 4 diego

--- soundspeed-scripts/kernels-fullnum/numerical-integrals/runjob.sh
+++ soundspeed-scripts/kernels-fullnum/numerical-integrals/runjob.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #
-#SBATCH --job-name=dT13
+#SBATCH --job-name=4diegodT22
 
 #SBATCH --mail-type=END
 #SBATCH --mail-user=fverdian@sissa.it
 #
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=56
+#SBATCH --cpus-per-task=36
 
 #SBATCH --time=40:00:00  
 #
@@ -14,4 +14,4 @@
 #SBATCH --output=Slurm-output/%x.o%j
 export OMP_NUM_THREADS=$((${SLURM_CPUS_PER_TASK}/2))
 
-python checks-Theta/break_dT_13.py -fx 0.1 -rtol 0.001 -N 1000 -kref 0.08
+python /home/fverdian/class/soundspeed-scripts/kernels-fullnum/numerical-integrals/4diego-theta-z4/break_dT_22.py -fx 0.1 -rtol 0.001 -N 1000 -kref 0.45
